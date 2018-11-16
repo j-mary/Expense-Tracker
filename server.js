e6143e3e96a8f2a48a5f8cb90bdf73cc1250c3a9
Jude Ocheje <ochejejude@gmail.com>
2018-11-16 13:35:33 +0100

    WIP

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+import config from 'config';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import error from './server/middleware/error';
 import expenses from './server/routes/expense-routes';
+import users from './server/routes/user-routes';
+import auth from './server/routes/auth';
 import 'express-async-errors';
-
 const app = express();
+
+// if (!config.get('jwtPrivateKey')) {
+//   console.error('FATAL ERROR: jwtPrivateKey is not defined');
+//   process.exit(1)
+// }
 
 mongoose
   .connect(
     'mongodb://localhost:27017/expense-tracker',
-    { useNewUrlParser: true }
+    { useCreateIndex: true, useNewUrlParser: true }
   )
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.error('Failed to connect to MongoDB...'));
@@ -18,6 +25,8 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use('/api/expenses', expenses);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 app.use(error);
 
 const port = process.env.PORT || 3000;
---
+++ server/routes/auth.js
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+import Joi from 'joi';
+import bcrypt from 'bcrypt';
+import { User } from '../model/user-model';
+import express from 'express';
+const router = express.Router()
+
+router.post('/', async (req, res) => {
+  // validate user input
+  const { error } = validateRequest(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+  // check if user exists via email
+  const user = await User.findOne({ email: req.body.email })
+  if (!user) return res.status(400).send('Invalid email or password')
+  // validate user password
+  const validPassword = await bcrypt.compare(req.body.password, user.password)
+  if (!validPassword) return res.status(400).send('Invalid email or password')
+  // if all went well, return token
+  const token = user.generateAuthToken()
+  res.send(token)
+})
+
+function validateRequest(req) {
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(6).max(255).required()
+  }
+
+  return Joi.validate(req, schema)
+}
+
+export default router
